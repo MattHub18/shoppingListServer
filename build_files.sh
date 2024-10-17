e9ade1d9c91cd415b2757e0df8dd745d4110efac
MattHub18 <70580254+MattHub18@users.noreply.github.com>
2024-10-17 19:17:32 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 python3 -m pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py makemigrations api
+python3 manage.py migrate api
 python3 manage.py collectstatic --noinput
