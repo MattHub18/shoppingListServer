99b6f47a6f3a771ae778964086c0f857578d5c67
Matteo <matteo.naldi1998@gmail.com>
2024-10-17 17:48:27 +0200

    before vercel

--- sl_api/settings.py
+++ sl_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-HOST = "vercel.app"
+HOST = ".vercel.app"
 
 ALLOWED_HOSTS = [HOST, "127.0.0.1", "localhost"]
 
